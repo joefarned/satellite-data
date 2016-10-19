4f304cd79cbc01086363fd50be7ad5115af21c70
Joe Farned <joefarned@gmail.com>
2016-10-19 12:56:05 -0400

    complete

--- analyze.py
+++ analyze.py
@@ -4,5 +4,84 @@ import os
 from osgeo import gdal
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
+from matplotlib import pyplot as plt
+
+raster_data_path = "data/image/2298119ene2016recorteTT.tif"
+output_fname = "classification.tiff"
+train_data_path = "data/test/"
+validation_data_path = "data/train/"
 
 COLORS = ["#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941"]
+
+def create_mask_from_vector(vector_data_path, cols, rows, geo_transform,
+                            projection, target_value = 1):
+    """Rasterize the given vector"""
+    # Open vector as GDALDataset
+    data_source = gdal.OpenEx(vector_data_path, gdal.OF_VECTOR)
+    layer = data_source.GetLayer(0);
+    driver = gdal.GetDriverByName('MEM') # Compute the raster result in memory
+    target_ds = driver.Create('', cols, rows, 1, gdal.GDT_UInt16)
+    target_ds.SetGeoTransform(geo_transform) # Map image
+    target_ds.SetProjection(projection) # Set location
+    gdal.RasterizeLayer(target_ds, [1], layer, burn_values = [target_value])
+    return target_ds
+
+def vectors_to_raster(file_paths, rows, cols, geo_transform, projection):
+    """Rasterize the vectors into a single image"""
+    labeled_pixels = np.zeros((rows, cols))
+    for i, path in enumerate(file_paths):
+        label = i + 1
+        ds = create_mask_from_vector(path, cols, rows, geo_transform,
+                                    projection, target_value=label)
+        band = ds.GetRasterBand(1)
+        labeled_pixels += band.ReadAsArray()
+        ds = None
+    return labeled_pixels
+
+# Read image into array 7 x row x col array
+raster_dataset = gdal.Open(raster_data_path, gdal.GA_ReadOnly)
+geo_transform = raster_dataset.GetGeoTransform()
+proj = raster_dataset.GetProjectionRef()
+bands_data = []
+for b in range(1, raster_dataset.RasterCount + 1):
+    band = raster_dataset.GetRasterBand(b)
+    bands_data.append(band.ReadAsArray())
+
+bands_data = np.dstack(bands_data)
+rows, cols, n_bands = bands_data.shape
+
+# Rasterize shape files, and produce row x col array
+files = [f for f in os.listdir(train_data_path) if f.endswith('.shp')]
+classes = [f.split('.')[0] for f in files]
+shapefiles = [os.path.join(train_data_path, f)
+              for f in files if f.endswith('.shp')]
+labeled_pixels = vectors_to_raster(shapefiles, rows, cols, geo_transform,
+                                   proj)
+
+# Prepare data
+is_train = np.nonzero(labeled_pixels)
+training_labels = labeled_pixels[is_train]
+training_samples = bands_data[is_train]
+
+# Run classifier
+classifier = RandomForestClassifier(n_jobs=-1)
+classifier.fit(training_samples, training_labels)
+
+# Predict
+n_samples = rows*cols
+flat_pixels = bands_data.reshape((n_samples, n_bands))
+result = classifier.predict(flat_pixels)
+classification = result.reshape((rows, cols))
+
+# Show prediction results
+f = plt.figure()
+f.add_subplot(1, 2, 2)
+r = bands_data[:,:,3]
+g = bands_data[:,:,2]
+b = bands_data[:,:,1]
+rgb = np.dstack([r,g,b])
+f.add_subplot(1, 2, 1)
+plt.imshow(rgb/255)
+f.add_subplot(1, 2, 2)
+plt.imshow(classification)
+plt.show()
